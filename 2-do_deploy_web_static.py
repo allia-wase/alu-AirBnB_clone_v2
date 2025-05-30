f23bc759a0e52d21db3da2900269db991219b7f1
allia-wase <a.umutoniwa1@alustudent.com>
2025-05-30 15:39:48 -0700

    Deploy archive to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Module: 2-do_deploy_web_static.py
+Function to deploy web_static archive to web servers.
 """
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['34.227.11.202', '52.90.153.53']
+from fabric.api import env, put, run
+import os
 
+env.hosts = ['<IP_WEB_01>', '<IP_WEB_02>']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Distributes an archive to your web servers.
+
+    Args:
+        archive_path (str): Path to the archive on the local machine.
+
+    Returns:
+        bool: True if all operations have been done correctly, False otherwise.
+    """
+    if not os.path.isfile(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        filename = os.path.basename(archive_path)          # web_static_20240610231825.tgz
+        folder_name = filename.replace(".tgz", "")         # web_static_20240610231825
+
+        # Upload archive to remote /tmp/
+        put(archive_path, "/tmp/{}".format(filename))
+
+        # Create release folder
+        run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
+
+        # Uncompress archive
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename, folder_name))
+
+        # Remove archive
+        run("rm /tmp/{}".format(filename))
+
+        # Move contents out of nested web_static
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(folder_name, folder_name))
+
+        # Remove redundant web_static folder
+        run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
+
+        # Delete current symlink
+        run("rm -rf /data/web_static/current")
+
+        # Create new symlink
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(folder_name))
+
+        print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
+        print("Error:", e)
         return False
